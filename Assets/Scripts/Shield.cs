dc71279293346cd4a409d192fabee045688f9489
tnargy <clsscmstng@gmail.com>
2020-07-27 08:45:49 -0500

    Shield animation

--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -4,12 +4,19 @@ public class Shield : MonoBehaviour
 {
     public int health;
     public float timeLeftInShield;
+    private SpriteRenderer sr;
+    private Material hitEffect;
+    private Material matDefault;
 
     // Start is called before the first frame update
     void OnEnable()
     {
         health = 5;
         timeLeftInShield = 10f;
+
+        sr = gameObject.GetComponent<SpriteRenderer>();
+        hitEffect = Resources.Load("WhiteFlash", typeof(Material)) as Material;
+        matDefault = sr.material;
     }
 
     // Update is called once per frame
@@ -33,8 +40,15 @@ public class Shield : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Bullet"))
         {
+            sr.material = hitEffect;
+            Invoke("RestMaterial", .1f);
             Destroy(collision.gameObject);
             health -= collision.GetComponent<Bullet>().damage;
         }
+    }
+
+    private void RestMaterial()
+    {
+        sr.material = matDefault;
     }
 }
